d04717eff9e70d2975517203955deb4c79531e1c
huyqd <quanghuy.dang210@gmail.com>
2023-07-03 20:11:24 +0200

    update vlae

--- generativedl/models/latent/vlae.py
+++ generativedl/models/latent/vlae.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 
 class ConvEncoder(nn.Module):
@@ -45,33 +46,120 @@ class ConvDecoder(nn.Module):
         return self.net(x)
 
 
-class VLAE(nn.Module):
-    def __init__(self, in_dim, latent_dim):
+class MaskedLinear(nn.Linear):
+    """ same as Linear except has a configurable mask on the weights """
+
+    def __init__(self, in_features, out_features, bias=True, conditional_size=None):
+        super().__init__(in_features, out_features, bias)
+        self.register_buffer('mask', torch.ones(out_features, in_features))
+
+        if conditional_size is not None:
+            self.cond_op = nn.Linear(conditional_size, out_features)
+
+    def set_mask(self, mask):
+        self.mask.data.copy_(torch.from_numpy(mask.astype(np.uint8).T))
+
+    def forward(self, input, cond=None):
+        out = F.linear(input, self.mask * self.weight, self.bias)
+        if cond is not None:
+            out = out + self.cond_op(input)
+        return out
+
+
+class MADE(nn.Module):
+    def __init__(self, input_shape, d, hidden_size=[512, 512], ordering=None,
+                 conditional_size=None):
         super().__init__()
-        self.latent_dim = latent_dim
-        self.encoder = ConvEncoder(in_dim=in_dim, latent_dim=latent_dim)
-        self.decoder = ConvDecoder(latent_dim=latent_dim, out_dim=in_dim)
+        self.input_shape = input_shape
+        self.nin = np.prod(input_shape)
+        self.nout = self.nin * d
+        self.d = d
+        self.hidden_sizes = hidden_size
+        self.ordering = np.arange(self.nin) if ordering is None else ordering
+
+        # define a simple MLP neural net
+        self.net = []
+        hs = [self.nin] + self.hidden_sizes + [self.nout]
+        for h0, h1 in zip(hs, hs[1:]):
+            self.net.extend([
+                MaskedLinear(h0, h1, conditional_size=conditional_size),
+                nn.ReLU(),
+            ])
+        self.net.pop()  # pop the last ReLU for the output layer
+        self.net = nn.ModuleList(self.net)
+
+        self.m = {}
+        self.create_mask()  # builds the initial self.m connectivity
+
+    def create_mask(self):
+        L = len(self.hidden_sizes)
+
+        # sample the order of the inputs and the connectivity of all neurons
+        self.m[-1] = self.ordering
+        for l in range(L):
+            self.m[l] = np.random.randint(self.m[l - 1].min(),
+                                          self.nin - 1, size=self.hidden_sizes[l])
+
+        # construct the mask matrices
+        masks = [self.m[l - 1][:, None] <= self.m[l][None, :] for l in range(L)]
+        masks.append(self.m[L - 1][:, None] < self.m[-1][None, :])
+
+        masks[-1] = np.repeat(masks[-1], self.d, axis=1)
+
+        # set the masks in all MaskedLinear layers
+        layers = [l for l in self.net.modules() if isinstance(l, MaskedLinear)]
+        for l, m in zip(layers, masks):
+            l.set_mask(m)
+
+    def forward(self, x, cond=None):
+        batch_size = x.shape[0]
+        out = x.view(batch_size, self.nin)
+        for layer in self.net:
+            if isinstance(out, MaskedLinear):
+                out = layer(out, cond=cond)
+            else:
+                out = layer(out)
+        out = out.view(batch_size, self.nin, self.d)
+        return out
+
+
+class AFVAE(nn.Module):
+    def __init__(self, input_shape, latent_size):
+        super().__init__()
+        assert len(input_shape) == 3
+        self.input_shape = input_shape
+        self.latent_size = latent_size
+
+        self.made = MADE(latent_size, 2, hidden_size=[512, 512])
+        self.encoder = ConvEncoder(input_shape[0], latent_size)
+        self.decoder = ConvDecoder(latent_size, input_shape[0])
 
     def loss(self, x):
-        x = 2 * x - 1
+        x = 2 * x.float() - 1
         mu_z, log_std_z = self.encoder(x)
         z = torch.randn_like(mu_z) * log_std_z.exp() + mu_z
-        x_hat = self.decoder(z)
+        x_recon = self.decoder(z)
 
-        # Compute reconstruction loss - Note that it may be easier for you
-        # to use torch.distributions.normal to compute the log_prob
-        recon_loss = F.mse_loss(x_hat, x, reduction='none').view(x.shape[0], -1).sum(1).mean()
+        recon_loss = F.mse_loss(x_recon, x, reduction='none').view(x.shape[0], -1).sum(1).mean()
+        enc_log_prob = -0.5 * np.log(2 * np.pi) - log_std_z - 0.5 * (z - mu_z) ** 2 * torch.exp(-2 * log_std_z)
 
-        # Compute KL
-        kl_loss = -log_std_z - 0.5 + (torch.exp(2 * log_std_z) + mu_z ** 2) * 0.5
-        kl_loss = kl_loss.sum(1).mean()
+        out = self.made(z)
+        mu, log_std = out.chunk(2, dim=-1)
+        log_std = torch.tanh(log_std)
+        mu, log_std = mu.squeeze(-1), log_std.squeeze(-1)
+        eps = z * torch.exp(log_std) + mu
+        prior_log_prob = -0.5 * np.log(2 * np.pi) + log_std - 0.5 * eps ** 2
 
-        return OrderedDict(
-            loss=recon_loss + kl_loss, recon_loss=recon_loss, kl_loss=kl_loss
-        )
+        kl_loss = (enc_log_prob - prior_log_prob).sum(1).mean()
+        return OrderedDict(loss=recon_loss + kl_loss, recon_loss=recon_loss,
+                           kl_loss=kl_loss)
 
-    def sample(self, n, noise=True):
+    def sample(self, n):
         with torch.no_grad():
-            z = torch.randn(n, self.latent_dim).cuda()
-            samples = torch.clamp(self.decoder(z), -1, 1)
-        return samples.cpu().permute(0, 2, 3, 1).numpy() * 0.5 + 0.5
+            z = torch.randn(n, self.latent_size).cuda()
+            for i in range(self.latent_size):
+                mu, log_std = self.made(z)[:, i].chunk(2, dim=-1)
+                log_std = torch.tanh(log_std)
+                mu, log_std = mu.squeeze(-1), log_std.squeeze(-1)
+                z[:, i] = (z[:, i] - mu) * torch.exp(-log_std)
+            return self.decoder(z).cpu().permute(0, 2, 3, 1).numpy() * 0.5 + 0.5
