375e2cf0a980870d5bdfa2f6ce9f4aa9d6d5aa13
Huy Dang <quanghuy.dang210@gmail.com>
2024-02-04 19:40:51 +0100

    set up cifar

---
+++ cifar/data.py
@@ -0,0 +1,43 @@
+import torch
+from torch.utils import data as data
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+
+from cifar.utils import set_seed
+
+DATASET_PATH = "../data"
+
+
+def load_data():
+    train_dataset = CIFAR10(root=DATASET_PATH, train=True, download=True)
+    DATA_MEANS = (train_dataset.data / 255.0).mean(axis=(0, 1, 2))
+    DATA_STD = (train_dataset.data / 255.0).std(axis=(0, 1, 2))
+
+    test_transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize(DATA_MEANS, DATA_STD)
+                                         ])
+    # For training, we add some augmentation. Networks are too powerful and would overfit.
+    train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                          transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(DATA_MEANS, DATA_STD)
+                                          ])
+    # Loading the training dataset. We need to split it into a training and validation part
+    # We need to do a little trick because the validation set should not use the augmentation.
+    train_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=train_transform, download=True)
+    val_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=test_transform, download=True)
+    set_seed(42)
+    train_set, _ = torch.utils.data.random_split(train_dataset, [45000, 5000])
+    set_seed(42)
+    _, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
+
+    # Loading the test set
+    test_set = CIFAR10(root=DATASET_PATH, train=False, transform=test_transform, download=True)
+
+    # We define a set of data loaders that we can use for various purposes later.
+    train_loader = data.DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True,
+                                   num_workers=4)
+    val_loader = data.DataLoader(val_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+    test_loader = data.DataLoader(test_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+
+    return train_loader, val_loader, test_loader
---
+++ cifar/utils.py
@@ -0,0 +1,34 @@
+import random
+
+import numpy as np
+import torch
+import torchvision
+from PIL import Image
+from matplotlib import pyplot as plt
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+
+def display_images(train_dataset, test_transform):
+    NUM_IMAGES = 4
+    images = [train_dataset[idx][0] for idx in range(NUM_IMAGES)]
+    orig_images = [Image.fromarray(train_dataset.data[idx]) for idx in range(NUM_IMAGES)]
+    orig_images = [test_transform(img) for img in orig_images]
+
+    img_grid = torchvision.utils.make_grid(torch.stack(images + orig_images, dim=0), nrow=4, normalize=True,
+                                           pad_value=0.5)
+    img_grid = img_grid.permute(1, 2, 0)
+
+    plt.figure(figsize=(8, 8))
+    plt.title("Augmentation examples on CIFAR10")
+    plt.imshow(img_grid)
+    plt.axis('off')
+    plt.show()
+    plt.close()
