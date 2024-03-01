a0ae9735fd1b343405f2e2601b9bdc2f639be226
Huy Dang <quanghuy.dang210@gmail.com>
2024-03-01 17:54:17 +0100

    rename

--- training/autoregressive/trainer.py
+++ training/autoregressive/trainer.py
@@ -1,11 +1,16 @@
 import os
 
 import lightning as L
+import numpy as np
 import torch
 from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
 from lightning.pytorch.loggers import WandbLogger
+from torch import nn, optim
+from torch.nn import functional as F
+from tqdm import tqdm
 
-from training.autoregressive.models.pixelcnn import PixelCNN
+from training.autoregressive.models.masked import VerticalMaskedConvolution, HorizontalMaskedConvolution
+from training.autoregressive.models.pixelcnn import GatedMaskedConv
 
 wandb_logger = WandbLogger(log_model=False, project="autoregressive", save_dir="../../assets/logs/")
 
@@ -20,6 +25,107 @@ elif torch.backends.mps.is_available():
 else:
     DEVICE = torch.device("cpu")
     ACCELERATOR = "cpu"
+
+
+class ARModule(L.LightningModule):
+    def __init__(self, c_in, c_hidden):
+        super().__init__()
+        self.save_hyperparameters()
+
+        # Initial convolutions skipping the center pixel
+        self.conv_vstack = VerticalMaskedConvolution(c_in, c_hidden, mask_center=True)
+        self.conv_hstack = HorizontalMaskedConvolution(c_in, c_hidden, mask_center=True)
+        # Convolution block of PixelCNN. We use dilation instead of downscaling
+        self.conv_layers = nn.ModuleList(
+            [
+                GatedMaskedConv(c_hidden),
+                GatedMaskedConv(c_hidden, dilation=2),
+                GatedMaskedConv(c_hidden),
+                GatedMaskedConv(c_hidden, dilation=4),
+                GatedMaskedConv(c_hidden),
+                GatedMaskedConv(c_hidden, dilation=2),
+                GatedMaskedConv(c_hidden),
+            ]
+        )
+        # Output classification convolution (1x1)
+        self.conv_out = nn.Conv2d(c_hidden, c_in * 256, kernel_size=1, padding=0)
+
+        self.example_input_array = torch.randn(1, 1, 28, 28)
+
+    def forward(self, x):
+        """Forward image through model and return logits for each pixel.
+
+        Args:
+            x: Image tensor with integer values between 0 and 255.
+        """
+        # Scale input from 0 to 255 back to -1 to 1
+        x = (x.float() / 255.0) * 2 - 1
+
+        # Initial convolutions
+        v_stack = self.conv_vstack(x)
+        h_stack = self.conv_hstack(x)
+        # Gated Convolutions
+        for layer in self.conv_layers:
+            v_stack, h_stack = layer(v_stack, h_stack)
+        # 1x1 classification convolution
+        # Apply ELU before 1x1 convolution for non-linearity on residual connection
+        out = self.conv_out(F.elu(h_stack))
+
+        # Output dimensions: [Batch, Classes, Channels, Height, Width]
+        out = out.reshape(out.shape[0], 256, out.shape[1] // 256, out.shape[2], out.shape[3])
+        return out
+
+    def calc_likelihood(self, x):
+        # Forward pass with bpd likelihood calculation
+        pred = self.forward(x)
+        nll = F.cross_entropy(pred, x, reduction="none")
+        bpd = nll.mean(dim=[1, 2, 3]) * np.log2(np.exp(1))
+        return bpd.mean()
+
+    @torch.no_grad()
+    def sample(self, img_shape, img=None):
+        """Sampling function for the autoregressive model.
+
+        Args:
+            img_shape: Shape of the image to generate (B,C,H,W)
+            img (optional): If given, this tensor will be used as
+                             a starting image. The pixels to fill
+                             should be -1 in the input tensor.
+        """
+        # Create empty image
+        if img is None:
+            img = torch.zeros(img_shape, dtype=torch.long).to(DEVICE) - 1
+        # Generation loop
+        for h in tqdm(range(img_shape[2]), leave=False):
+            for w in range(img_shape[3]):
+                for c in range(img_shape[1]):
+                    # Skip if not to be filled (-1)
+                    if (img[:, c, h, w] != -1).all().item():
+                        continue
+                    # For efficiency, we only have to input the upper part of the image
+                    # as all other parts will be skipped by the masked convolutions anyways
+                    pred = self.forward(img[:, :, : h + 1, :])
+                    probs = F.softmax(pred[:, :, c, h, w], dim=-1)
+                    img[:, c, h, w] = torch.multinomial(probs, num_samples=1).squeeze(dim=-1)
+        return img
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        scheduler = optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.99)
+        return [optimizer], [scheduler]
+
+    def training_step(self, batch, batch_idx):
+        loss = self.calc_likelihood(batch[0])
+        self.log("train_bpd", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.calc_likelihood(batch[0])
+        self.log("val_bpd", loss)
+
+    def test_step(self, batch, batch_idx):
+        loss = self.calc_likelihood(batch[0])
+        self.log("test_bpd", loss)
 
 
 def train_autoregressive(train_loader, val_loader, test_loader, **kwargs):
@@ -40,11 +146,11 @@ def train_autoregressive(train_loader, val_loader, test_loader, **kwargs):
     pretrained_filename = os.path.join(CHECKPOINT_PATH, "PixelCNN.ckpt")
     if os.path.isfile(pretrained_filename):
         print("Found pretrained model, loading...")
-        model = PixelCNN.load_from_checkpoint(pretrained_filename)
+        model = ARModule.load_from_checkpoint(pretrained_filename)
         ckpt = torch.load(pretrained_filename, map_location=DEVICE)
         result = ckpt.get("result", None)
     else:
-        model = PixelCNN(**kwargs)
+        model = ARModule(**kwargs)
         trainer.fit(model, train_loader, val_loader)
     model = model.to(DEVICE)
 
